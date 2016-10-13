ff68e5539d67b4c5a1aee3c111fe72ae2ed459f9
Harlinton <ton_mangueira@hotmail.com>
2016-10-13 20:43:22 -0300

    Commit First

---
+++ Siapen.SystemVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Siapen.SystemVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestSiapen
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Siapen.SystemVirtual.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Siapen.SystemVirtual.Web.MvcApplication" Language="C#" %>
